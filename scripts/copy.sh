ab4496b7566326ca36aa446ba159516e50ba8746
tryecho <your_email@example.com>
2023-05-05 16:47:44 -0500

    changed directory order and added comment on copy scriptç

--- scripts/copy.sh
+++ scripts/copy.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-
+#use -rn to copy without replacing the files, can be used if copy fails. 
 cp -rv /mnt/udisk/web0/* /mnt/web0 >> copy_$(date +%Y-%m-%d)_$(cat /tmp/device.info | grep Device_SN | awk -F'=' '{print  $2}' | tr -d ' ').log &
