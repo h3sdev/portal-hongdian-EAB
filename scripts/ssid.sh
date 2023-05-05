415aac26943dca50074f9589f75b95c328c028ae
tryecho <your_email@example.com>
2023-05-05 17:03:39 -0500

    Changes on SSID script to add variable to pick the ssid

--- scripts/ssid.sh
+++ scripts/ssid.sh
@@ -6,7 +6,7 @@ WiFiSSID="CopetranWiFi"
 #Assign the variables from command-line arguments
 boxID="$(cat /tmp/device.info | grep Device_SN | awk -F'=' '{print  $2}'| tail -c 4 | tr -d ' ')"
 
-#agrega el número de la caja y renombra por el ssid elegido
+#adds the box number to the SSID and renames it to the chosen SSID
 replace=" ssid $WiFiSSID_$boxID"
 
 #Replace the whole line where the search string is found with the value of replace in the file
@@ -15,11 +15,11 @@ file="/etc/hdconfig/cli.conf"
 search="ssid"
 sed -i "/$search/c $replace" $file
 
-#Quitar la clave del la red wifi removiendo toda la linea que contiene la palabra seguridad
+#Removes the Wi-Fi network key by removing the whole line that contains the "security" string
 remove="security"
 sed -i "/$remove/d" $file
 
-#Actualizar la direccion ip del servidor remoto de contenido (ya no se usa, pero en caso de emergencia puede ser útil)
+#Updates the IP address of the remote content server (no longer used, but may be useful in case of emergency)
 search="update_server"
 replaceIp="update server 34.198.242.94:8888/freewifi"
 sed -i "/$search/c $replaceIp" $file
